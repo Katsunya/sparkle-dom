8bb33de1e71c4b21373970262c630e1eb134e3f7
Katsunya <38696161+Katsunya@users.noreply.github.com>
2018-12-30 02:26:22 -0500

    Create snowfall.js

---
+++ snowfall.js
@@ -0,0 +1,4 @@
+$.getScript("http://cople.github.io/Snowfall.js/snowfall.js", function() {
+  console.log("Loaded in snow.");
+});
+$(document).ready(()=>{$("body").prepend("<link href='http://cople.github.io/Snowfall.js/snowfall.css' rel=\"stylesheet\">");
