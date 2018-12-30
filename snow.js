68451244f67b3f8359b437386a0102ec4f4851f9
Katsunya <38696161+Katsunya@users.noreply.github.com>
2018-12-30 02:04:22 -0500

    Update snow.js

--- snow.js
+++ snow.js
@@ -1,4 +1,4 @@
 $.getScript("https://Katsunya.github.io/sparkle-dom/snowfall.css", function() {
   console.log("Loaded in snow.");
 });
-$(document).ready(()=>{$("body").prepend("<link href='https://Katsunya.github.io/sparkle-dom/snowfall.css' rel=\"stylesheet\">");};
+$(document).ready(()=>{$("body").prepend("<link href='https://Katsunya.github.io/sparkle-dom/snowfall.css' rel=\"stylesheet\">");});
