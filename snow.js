aedd6872ab66b3ee953f854fea2b647eee2bc78c
Katsunya <38696161+Katsunya@users.noreply.github.com>
2018-12-30 01:57:19 -0500

    Update snow.js

--- snow.js
+++ snow.js
@@ -1,4 +1,4 @@
 $.getScript("https://ellexidecodes.github.io/Profile-Codes/repeatedEffects/snowfall.js", function() {
   console.log("Loaded in snow.");
 });
-$(document).ready(()=>{$("body").prepend("<link href='https://ellexidecodes.github.io/Profile-Codes/repeatedEffects/snowfall.css' rel=\"stylesheet\">");
+$(document).ready(()=>{$("body").prepend("<link href='https://github.com/Katsunya/sparkle-dom/blob/master/snowfall.css' rel=\"stylesheet\">");
