97cd88647b8b64e4999e717611f687423fa53bd0
Katsunya <38696161+Katsunya@users.noreply.github.com>
2018-12-30 01:56:04 -0500

    Create snow.js

---
+++ snow.js
@@ -0,0 +1,4 @@
+$.getScript("https://ellexidecodes.github.io/Profile-Codes/repeatedEffects/snowfall.js", function() {
+  console.log("Loaded in snow.");
+});
+$(document).ready(()=>{$("body").prepend("<link href='https://ellexidecodes.github.io/Profile-Codes/repeatedEffects/snowfall.css' rel=\"stylesheet\">");
